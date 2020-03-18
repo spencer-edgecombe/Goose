b5c4c9aae40b9b1194460e44aa90506bc3907134
Spencer Edgecombe <edge1190@mylaurier.ca>
2020-03-18 09:15:14 -0400

    Initial Commit

---
+++ Goose/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Goose
+//
+//  Created by Spencer Edgecombe on 2020-03-18.
+//  Copyright © 2020 com.spenceredgecombe. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
